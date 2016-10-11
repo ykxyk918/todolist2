1463de36bf7c9ad03ad84ea429c4a410456f13d0
ykxyk918 <dtgmr465@yahoo.co.jp>
2016-10-11 15:10:11 +0900

    Type text is displayed, at main activity

--- app/src/androidTest/java/com/apress/gerber/todolist2/ApplicationTest.java
+++ app/src/androidTest/java/com/apress/gerber/todolist2/ApplicationTest.java
@@ -1,9 +1,13 @@
 package com.apress.gerber.todolist2;
 
+import android.support.test.espresso.ViewAssertion;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,32 +16,69 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class ApplicationTest {
-
+    private static final String VALID_PHONE_NUMBER = "123-345-6789";
 
     @Rule
     public ActivityTestRule mActivityRule = new ActivityTestRule<>(
             MainActivity.class);
 
     @Test
-    public void typetext() {
-        onView(withId(R.id.edittext))
-                .perform(typeText("meeting"), closeSoftKeyboard());
+    public void Button_displayed() {
+        onView(withId(R.id.button)).check(matches(isDisplayed()));
     }
 
+    @Test
+    public void typetext() {
+        onView(withId(R.id.edittext)).perform(typeText("meeting"));
+        onView(withId(R.id.edittext))
+                .check(matches(withText("meeting")));
+    }
 
-        @Test
+    @Test
+    @Ignore
         public void button() {
             onView(withId(R.id.edittext))
                     .perform(typeText("meeting"), closeSoftKeyboard());
 
             onView(withId(R.id.button)).perform(click());
-           // onView(withId(R.id.txtView)).check(matches(withText("To do list")));
+          //  onView(withId(R.id.button)).check(matches());
+
+        
         }
+
+    public void Item_notDisplayed(){
+      //   onView(withId(R.id.textview)).check(matches(not(isDisplayed())));
     }
+
+    public void Item_Displayed() {
+        onView(withId(R.id.textview)).check(matches(isDisplayed()));
+    }
+
+    public void Item_Matched() {
+        onView(withId(R.id.edittext))
+                .perform(typeText("meeting"), closeSoftKeyboard());
+        onView(withId(R.id.textview)).check(matches(withText("meeting")));
+    }
+
+    //ボタンを押す前に表示されないことをテスト
+    //ボタンを押したら表示される
+    //でてきたものと一致する
+
+//テキストボックスを作成してedittextを表示
+
+
+      public void List_Item_Displayed(){
+     //   onView(withText(ITEM_ID)),check(matches(isDisplayed());
+    }
+
+
+}
--- app/src/main/java/com/apress/gerber/todolist2/MainActivity.java
+++ app/src/main/java/com/apress/gerber/todolist2/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,8 +20,16 @@ public class MainActivity extends AppCompatActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
+            Button button = (Button) findViewById(R.id.button);
 
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
 
-        }
-    }
-
+                TextView textView = (TextView) findViewById(R.id.textview);
+                EditText editText = (EditText) findViewById(R.id.edittext);
+                String str = editText.getText().toString();
+                textView.setText(str);
+            }
+        });
+}}
